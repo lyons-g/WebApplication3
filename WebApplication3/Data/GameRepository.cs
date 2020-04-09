aaea82bfb49b8138a53dd29fbdb4bd9f08e6e4b9
lyons-g <62383079+lyons-g@users.noreply.github.com>
2020-04-09 16:48:23 +0100

    IGameRepo and GameRepo added

---
+++ WebApplication3/Data/GameRepository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class GameRepository : IGameRepository, IDisposable
+    {
+        private ApplicationDbContext context;
+
+        public GameRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Game> GetGames()
+        {
+            return context.Games.ToList();
+        }
+
+        public Game GetGameByID(int GameId)
+        {
+            return context.Games.Find(GameId);
+        }
+
+        public void addGame (Game game)
+        {
+            context.Games.Add(game);
+        }
+
+        public void DeleteGame(int GameID)
+        {
+            Game game = context.Games.Find(GameID);
+                context.Games.Remove(game);
+        }
+
+        public void UpdateGame(Game game)
+        {
+            context.Entry(game).State = EntityState.Modified;
+        }
+
+        public void Save()
+            {
+            context.SaveChanges();
+        }
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ WebApplication3/Data/IGameRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public interface IGameRepository : IDisposable
+    {
+        IEnumerable<Game> GetGames();
+        Game GetGameByID(int GameId);
+        void addGame(Game game);
+        void DeleteGame(int GameId);
+        void UpdateGame(Game game);
+        void Save();
+
+    }
+}
